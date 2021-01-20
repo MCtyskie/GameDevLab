8f8ed813cf982f4f4ed1eed6144c94817cd9c3e5
MCtyskie <micsul000@utp.edu.pl>
2021-01-20 13:49:00 +0100

    added UnityMobile lab 2 task 1

---
+++ UnityMobile_lab2_task1/Assets/IAPManag.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class IAPManager : MonoBehaviour, IStoreListener
+{
+    public static IAPManager instance;
+
+    private static IStoreController m_StoreController;
+    private static IExtensionProvider m_StoreExtensionProvider;
+
+    //Step 1 create your products
+    public static string removeAds = "removeAds";
+    public static string coins100 = "grant 100 coins";
+    public static string premium = "premium";
+
+    //************************** Adjust these methods **************************************
+    public void InitializePurchasing()
+    {
+        if (IsInitialized()) { return; }
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        //Step 2 choose if your product is a consumable or non consumable
+        builder.AddProduct(removeAds, ProductType.NonConsumable);
+        builder.AddProduct(coins100, ProductType.Consumable);
+        builder.AddProduct(premium, ProductType.Subscription);
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    private bool IsInitialized()
+    {
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+    //Step 3 Create methods
+    public void BuyGrantCredits()
+    {
+        BuyProductID(coins100);
+    }
+    public void BuyRemoveAds()
+    {
+        BuyProductID(removeAds);
+    }
+    public void BuyPremium()
+    {
+        BuyProductID(premium);
+    }
+
+
+    //Step 4 modify purchasing
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, "removeAds", StringComparison.Ordinal))
+        {
+            Debug.Log("Usunięto reklamy!");
+        }
+        else if(String.Equals(args.purchasedProduct.definition.id, "coins100", StringComparison.Ordinal))
+        {
+            Debug.Log("Dodano 100 monet!");
+        }
+        else if(String.Equals(args.purchasedProduct.definition.id, "premium", StringComparison.Ordinal))
+        {
+            Debug.Log("Wykupiono miesięczną subskrypcję premium!");
+        }
+        else
+        {
+            Debug.Log("Purchase Failed");
+        }
+        return PurchaseProcessingResult.Complete;
+    }
+
+    //**************************** Dont worry about these methods ***********************************
+    private void Awake()
+    {
+        TestSingleton();
+    }
+
+    void Start()
+    {
+        if (m_StoreController == null) { InitializePurchasing(); }
+    }
+
+    private void TestSingleton()
+    {
+        if (instance != null) { Destroy(gameObject); return; }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void BuyProductID(string productId)
+    {
+        if (IsInitialized())
+        {
+            Product product = m_StoreController.products.WithID(productId);
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            {
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        else
+        {
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+    public void RestorePurchases()
+    {
+        if (!IsInitialized())
+        {
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            Debug.Log("RestorePurchases started ...");
+
+            var apple = m_StoreExtensionProvider.GetExtension<IAppleExtensions>();
+            apple.RestoreTransactions((result) => {
+                Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+            });
+        }
+        else
+        {
+            Debug.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+        }
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        Debug.Log("OnInitialized: PASS");
+        m_StoreController = controller;
+        m_StoreExtensionProvider = extensions;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+}
---
+++ UnityMobile_lab2_task1/Assets/PurchaseFulfillment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseFulfillment : MonoBehaviour
+{
+    public void grantCredits(int amount)
+    {
+        Debug.Log("Otrzymałeś "+amount+" monet!");
+    }
+    public void removeAds()
+    {
+        Debug.Log("Reklamy zostały usunięte!");
+    }
+    public void accessPremium()
+    {
+        Debug.Log("Otrzymałeś wersję premium gry!");
+    }
+}
